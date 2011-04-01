dcda5467ca3e52812267a871f374557e75c8e229
Kevin R. Barnes <vinbarnes@gmail.com>
2011-04-01 08:34:25 -0500

    Add imageTagForString function

--- emogc.js
+++ emogc.js
@@ -7,3 +7,9 @@
  * http://benalman.com/about/license/
  */
 (function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+
+function imageTagForString(str) {
+  // TODO what is the desired behavior if the image doesn't exist
+  
+  return "<img alt='" + str + "' title='" + str + "' src='https://github.com/flogic/emogc/raw/develop/images/" + str + ".png' />";
+}
